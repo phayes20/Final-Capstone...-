6b29fcd88a6533a53bd600153ea4b384535ccb8c
Kate Kohr <katekohr@icloud.com>
2022-04-11 12:14:42 -0400

    Capstone

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,3 +1,4 @@
+
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
@@ -23,3 +24,55 @@ INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpUL
 
 
 COMMIT TRANSACTION;
+
+
+
+
+BEGIN TRANSACTION;
+
+DROP TABLE IF EXISTS locations;
+DROP SEQUENCE IF EXISTS seq_location_id;
+
+CREATE SEQUENCE seq_location_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+
+CREATE TABLE locations (
+	location_id int DEFAULT nextval('seq_location_id'::regclass) NOT NULL,
+	name varchar(50) NOT NULL UNIQUE,
+	description varchar(200) NOT NULL,
+	sunday_hour_open time NOT NULL,
+	sunday_hour_closed time NOT NULL,
+	monday_hour_open time NOT NULL,
+	monday_hour_closed time NOT NULL,
+	tuesday_hour_open time NOT NULL,
+	tuesday_hour_closed time NOT NULL,
+	wednesday_hour_open time NOT NULL,
+	wednesday_hour_closed time NOT NULL,
+	thursday_hour_open time NOT NULL,
+	thursday_hour_closed time NOT NULL,
+	friday_hour_open time NOT NULL,
+	friday_hour_closed time NOT NULL,
+	saturday_hour_open time NOT NULL,
+	saturday_hour_closed time NOT NULL,
+	social_media varchar(100),
+	category varchar(50) NOT NULL,
+	CONSTRAINT PK_location PRIMARY KEY (location_id)
+);
+
+INSERT INTO locations (name, description, sunday_hour_open, sunday_hour_closed, monday_hour_open, monday_hour_closed, tuesday_hour_open, tuesday_hour_closed,
+wednesday_hour_open, wednesday_hour_closed, thursday_hour_open, thursday_hour_closed, friday_hour_open, friday_hour_closed, saturday_hour_open, saturday_hour_closed,social_media, category) 
+values ('test', 'test description', '09:00:00', '17:00:00', '09:00:00', '17:00:00','09:00:00', '17:00:00','09:00:00', '17:00:00','09:00:00', '17:00:00','09:00:00', '17:00:00','09:00:00', '17:00:00', 'www.Facebook.com', 'food');
+
+COMMIT TRANSACTION;
+
+
+
+
+
+
+
+
