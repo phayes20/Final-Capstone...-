7bb837325a7535fd192b1c0043eb54d4bbb1f9e4
Payton Picone <ppicone4@gmail.com>
2022-04-12 12:30:13 -0400

    start of front end

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <p>WalkCLE</p>
+    <!-- <img src="src\img\skyline.jpg" alt="Cleveland Skyline"> -->
+    
+    <aside>
+        <nav>
+            <ul>
+                <li><a href="#">Bars / Food</a></li>
+                <li><a href="#">Sports</a></li>
+                <li><a href="#">PlayHouse District</a></li>
+                <li><a href="#">Arts</a></li>
+                <li><a href="#">Parks</a></li>
+                <li><a href="#">Historical Sites</a></li>
+            </ul>
+        </nav>
+    </aside>
+  
+
+
+
+
   </div>
 </template>
 
@@ -10,3 +29,7 @@ export default {
   name: "home"
 };
 </script>
+
+
+
+<link rel="stylesheet" href="style.css">
