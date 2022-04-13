d95c0659b30c2de3e3f3d5dcba46b43daced09fc
Donald Sweeney <donsweeney88@gmail.com>
2022-04-13 14:06:35 +0000

    started front end design

---
+++ capstone/vue/src/components/Search.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="search">
+        <form>
+            <textarea> </textarea>
+        </form>
+        <form id="time-search">
+            <!-- <label for="day">Day:</label>
+            <select id="day">
+                <option value="Sunday">Sunday</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+                <option value="Saturday">Saturday</option>
+            </select>
+            <label for= "time"> 
+            <select id="time">time</select>
+        </form> -->
+        <input type="datetime-local" id="search-time"/>
+        </form>
+    </div>
+</template>
+
+<script>
+
+
+export default 
+    {
+       name: 'search-form' 
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ capstone/vue/src/services/LocationService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: "http://localhost:8080"
+});
+
+
+export default {
+
+    getLocations() {
+        return http.get('/location');
+    }
+
+}
