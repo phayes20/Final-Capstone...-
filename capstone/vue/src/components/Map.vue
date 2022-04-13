f6fa9ec9468fc81e8ce040022fddce659ce9cf40
Kate Kohr <katekohr@icloud.com>
2022-04-13 18:19:35 -0400

    front end stuff

--- capstone/vue/src/components/Map.vue
+++ capstone/vue/src/components/Map.vue
@@ -1,18 +1,41 @@
 <template>
     <div class="map">
+        <head>
+        <!-- <vue-headful
+        : -->
+       <meta charset="UTF-8"> 
+       <meta name= "viewport" content= "width=device-width, initial-scale=1.0">
+       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+       <title>My Google Maps</title>
+       </head>
+       <body>
+           <h1>My Google Map</h1>
+           <div id="map"></div>
+           <script>
+               function initMap() {
+                   var options = {
 
+                   }
 
+               }
+           </script> 
+           <script async defer  
+           src= "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&v=weekly">
+           </script> 
+           </body>   
+        <style>
+            
 
         </div>
 </template>
 
 <script>
 import { defineComponent } from '@vue/composition-api'
-
 export default {
     
 }
 
+ src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&v=weekly"
 
 </script>
 
