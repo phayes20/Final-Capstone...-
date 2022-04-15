7a9b322b25bf5983304238494cda7e8efc57275b
Payton Picone <ppicone4@gmail.com>
2022-04-15 17:30:07 -0400

    front end work

--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -8,22 +8,27 @@
     <add-google-map />
     <location-list />
     <div id="background_photo">
-      <img alt="background" src="../img/night skyline.jpg">
+      <img alt="background" src="../img/skyline.jpg">
     </div>
     <div id="food_photo">
-      <img alt="food" src="../img/food collage.jpeg">
+      <img src="../img/food collage.jpeg">
     </div>
   </div>
 </template>
 
-<!--relative path similar to line 24 -->
+
+body {background-image:url(“skyline.jpg”);
+      background-attachment: fixed;
+      background-position: center;
+      background-repeat: no-repeat;background-size: cover;}
+
+
 
 <script>
 
 import NavBar  from '@/components/NavBar';
 import Search from '@/components/Search';
 import AddGoogleMap from '@/components/AddGoogleMap';
-
 import LocationList from '../components/LocationList.vue';
 
 export default {
@@ -38,12 +43,19 @@ export default {
 
 <style>
 
+
 #food_photo {
   grid-area: Food_Img;
+  width: 10px;
+  height: 10px;
 }
 
 #Food {
-    grid-area: Food;
+  grid-area: Food;
+  width: 75px;
+  height: 75px; 
+  background-color: blue;
+
 }
 
 #Sports {
@@ -68,12 +80,14 @@ export default {
 
 ul {
     display: grid;
-    grid-template-rows: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     grid-template-areas:
-       "Food Food_Img Sports Sports"
-       "Theater Theater Arts Arts"
-       "Parks Parks Historical Historical"
+      ""
+      "Food Food_Img Sports Sports"
+      "Theater Theater Arts Arts"
+      "Parks Parks Historical Historical"
+      ""
        ;
     height: 100px;
     gap: 20px;
